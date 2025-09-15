07b395d631ebbaf3492e16dcfb8477b88a175bd9
EndersonAs <ariza.ambiente@gmail.com>
2025-09-15 15:23:41 -0500

    nuevo archivo de notebook

--- 1_intro.py
+++ 1_intro.py
@@ -1,17 +1,1 @@
-a = True 
-b = False
-
-d = 42
-e = "Hello, World!"
-f = 3.14
-
-print(a, b, d, e, f)
-
-print(a == f)
-print(a==b)
-print(d != f)
-print(d > f, "¿d es mayor que f?   ") 
-
-#otro ejemplo 
-
-
+print ("hello world!!")
