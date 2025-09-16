721261a7273704ce7349d65f5ad6afce83a1f26a
EndersonAs <ariza.ambiente@gmail.com>
2025-09-16 15:48:32 -0500

    ejercicios sencillo sobre operadores y comparadores

--- 11_booleans.py
+++ 11_booleans.py
@@ -3,3 +3,10 @@ print (type(is_single))
 
 is_single = False
 print (is_single)
+
+"""
+print(not True)
+print (not False) 
+"""
+is_single= not is_single # Cambia el valor de la variable a su opuesto con not
+print (is_single)
---
+++ 12_transformation.py
@@ -0,0 +1,25 @@
+name = "David"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = 12.5
+print(type(name))
+
+name = True
+print(type(name))
+
+print ( "david" + " " + "ariza") # Concatenacion de strings
+
+x = 12
+print ("este numero  es un ejemplo " + str(x))
+
+print (f"este numero es un ejemplo {x}") # f string
+
+edad =input ("Cual es tu edad? ") # input siempre devuelve un string
+print (type(edad))
+edad = int(edad) # Convertir string a entero
+print (type(edad))
+print (f"Tu edad es {edad + 10}")
+
