58553e9581934e40bbb5263ce1c42007efb7fee3
EndersonAs <ariza.ambiente@gmail.com>
2025-09-16 06:34:45 -0500

    ejercicios sencillos con numeros enteros

---
+++ 10_numbers.py
@@ -0,0 +1,29 @@
+lives = 3
+print (type(lives))
+age = 12
+buget = 100
+
+teperature = 36.6
+print (type(teperature))
+
+lives = 2 
+print (lives)
+
+lives = 1
+print (lives)
+
+lives = 12 + 15
+print (lives) 
+
+lives = lives - 1
+print (lives)
+
+lives -= 1 # una forma diferente de escribir lives = lives - 1
+print (lives)
+
+lives += 5
+print (lives)
+
+# un numero muy grande cuando es muy grande lo convierte en notacion cientifica
+number =45000000000000000000000000000000000000000000000000000.1 
+print (number)
