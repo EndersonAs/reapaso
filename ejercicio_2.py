3d9b927286210c7d46bdd62c921ed5de627a11d5
EndersonAs <ariza.ambiente@gmail.com>
2025-09-17 20:36:44 -0500

    ejercicio numero 2 en clase

---
+++ ejercicio_2.py
@@ -0,0 +1,27 @@
+# punto 1
+lista_1 = [24, 25, 35, 47, 58, 69, 58, 24, 12]
+print(lista_1)
+
+#punto 2
+print(lista_1[0], "primer elemento", lista_1[-1], "último elemento")
+
+#punto 3
+lista_1.append(72)
+print(lista_1) 
+
+#punto 4
+print(len(lista_1))
+
+#punto 5
+lista_1.sort()
+print("Este es elemento menor: ", lista_1[0], "Este es el elemento mayor: ", lista_1[-1])
+
+#punto 6
+suma = sum(lista_1)
+print("Suma total es: ", suma)
+
+#punto 7
+print(lista_1.count(58), "Repetidos") 
+
+#punto 8
+print("El índice es: ", lista_1.index(35))
