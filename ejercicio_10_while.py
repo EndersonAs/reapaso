7f26f80b70840326944650483736be2afcd767ec
EndersonAs <ariza.ambiente@gmail.com>
2025-09-20 00:09:43 -0500

    Ejercicio con while

---
+++ ejercicio_10_while.py
@@ -0,0 +1,7 @@
+#ingresar el numero 15 y que lo imprima de mayor a menor de 2 en 2 
+#Ejemplo -> (15, 13, 11, 9 ...)
+numero = int(input("ingrese un numero: "))
+
+while numero > 0:
+	print(numero)
+	numero -= 2
