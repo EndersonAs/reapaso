97767a64838a241aa29b9542ad349fbd90e3f910
EndersonAs <ariza.ambiente@gmail.com>
2025-09-20 21:17:23 -0500

    Aprendiendo el metodo format con .f y .g

--- ejercicio_8_for_2.py
+++ ejercicio_8_for_2.py
@@ -27,3 +27,10 @@ promedio_4 = sum_nota_4 / len(estudiantes["notas_4"])
 
 print(f"{promedio_1}, {promedio_2}, {promedio_3}, {promedio_4}")
 
+#diferentes print usando el metodo format => Usa .2f si quieres siempre 2 decimales exactos (bueno para dinero, promedios con exactitud fija, etc.).
+print(f"{promedio_1:.2f}, {promedio_2:.2f}, {promedio_3:.2f}, {promedio_4}") #👉 .2f significa: fijo con 2 decimales.
+print(f"{promedio_1:.1f}, {promedio_2:.1f}, {promedio_3:.1f}, {promedio_4}") #👉 .1f significa: fijo con 2 decimales.
+print(f"{promedio_1:.4f}, {promedio_2:.4f}, {promedio_3:.4f}, {promedio_4}") #👉 .3f significa: fijo con 2 decimales.
+
+print(f"{promedio_1:.2g}, {promedio_2:.2g}, {promedio_3:.2g}, {promedio_4}") #Usa .2g si quieres valores cortos, adaptativos (solo 2 cifras importantes).
+
