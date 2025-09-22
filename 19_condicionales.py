699029b803bd0d73f2d8956ffb50311ec979598f
EndersonAs <ariza.ambiente@gmail.com>
2025-09-22 15:08:53 -0500

    ejercicio con condicionales

---
+++ 19_condicionales.py
@@ -0,0 +1,26 @@
+if True:
+    print("deveria ejecutarse")
+
+if False:
+    print("nunca se ejetuta ") 
+
+pet = input("¿cual es tu mascota favorita? ")
+
+if pet == "perro":
+    print("te tienen de marrano")
+elif pet == "gato":
+    print("tienes suerte ")
+elif pet == "pez":
+    print("eres lo maximo")
+else:
+    print("no tienes una mascota valida")
+
+
+"""stock = input("digita el stock")
+stock =int(stock)
+
+if stock >= 100 and stock <= 1000:
+    print("el stock es correcto")
+else:
+    print("el stock es incorrecto")
+"""
