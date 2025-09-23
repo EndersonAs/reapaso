62f5c27deadb0ba09f94287b92f2892343d44101
EndersonAs <ariza.ambiente@gmail.com>
2025-09-23 14:35:03 -0500

    introduccion aerca de las listas

---
+++ 24_listas.py
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(type(numbers))
+
+tasks = ["amke a dishes", "play videogames"]
+print(tasks)
+
+types= [1, True, "hola"]
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+
+text = "hola"
+
+tasks[0] = "watch platzy"
+print(tasks)
+
+tasks[0] = "do the dishes"
+print(tasks)
+
+print(numbers[:3])
+print(True in types)
+  
