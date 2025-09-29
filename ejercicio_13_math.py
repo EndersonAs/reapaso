d69438c6497adc0d07b562f331ae7b4e7ce8f7f8
EndersonAs <ariza.ambiente@gmail.com>
2025-09-29 11:08:41 -0500

    Retomando ejercicios

---
+++ ejercicio_13_math.py
@@ -0,0 +1,19 @@
+#Algunos ejemplos con la libreria math
+
+import math
+
+print(math.sqrt(16)) #Raiz cuadrado
+print(math.log(100,10)) #logaritmos
+print(math.floor(3.7)) #redondeo hacia arriba o hacia abajo.
+print(math.ceil(3.7))
+print(math.pow(5,3)) #potencia
+
+print(math.tan(3.1416)) #tangente
+print(math.sin(math.pi/2)) #seno
+print(math.cos(math.pi/2)) #coseno
+
+#factorial
+print(math.factorial(5))
+print(math.factorial(100))
+
+
